21f35f987303e6f0a114c873baeafe29c414e5a9
Coby <coby.nelson@wsu.edu>
2022-12-14 06:18:28 +0000

    Added 63 files and function to call those files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,634 @@
 /* code for Meal Wizard */
+
+function start() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option1.html");
+  ajax.send();
+}
+/* level 1 selection options*/
+function makeFood() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option3.html");
+  ajax.send();
+}
+
+function eatOut() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option2.html");
+  ajax.send();
+}
+
+/* level 2 selection options*/
+function dineIn() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option4.html");
+  ajax.send();
+}
+
+function fastFood() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option6.html");
+  ajax.send();
+}
+
+function cookFood() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option5.html");
+  ajax.send();
+}
+
+function frozenFood() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option7.html");
+  ajax.send();
+}
+
+/* level 3 selection options*/
+function independent() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option8.html");
+  ajax.send();
+}
+
+function chain() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option10.html");
+  ajax.send();
+}
+
+function cookDinner() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option9.html");
+  ajax.send();
+}
+
+function cookBreakfast() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option11.html");
+  ajax.send();
+}
+
+function fastDinner() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option12.html");
+  ajax.send();
+}
+
+function fastBreakfast() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option14.html");
+  ajax.send();
+}
+
+function frozenDinner() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option13.html");
+  ajax.send();
+}
+
+function frozenBreakfast() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option15.html");
+  ajax.send();
+}
+
+/* level 4 selection options*/
+function sella() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option16.html");
+  ajax.send();
+}
+
+function birch() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option18.html");
+  ajax.send();
+}
+
+function american() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option17.html");
+  ajax.send();
+}
+
+function italian() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option19.html");
+  ajax.send();
+}
+
+function applebees() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option20.html");
+  ajax.send();
+}
+
+function buffalo() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option22.html");
+  ajax.send();
+}
+
+function protein() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option21.html");
+  ajax.send();
+}
+
+function sweet() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option23.html");
+  ajax.send();
+}
+
+function taco() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option24.html");
+  ajax.send();
+}
+
+function arbys() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option26.html");
+  ajax.send();
+}
+
+function frozenPizza() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option25.html");
+  ajax.send();
+}
+
+function frozenNuggets() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option27.html");
+  ajax.send();
+}
+
+function jack() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option28.html");
+  ajax.send();
+}
+
+function mcDonald() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option30.html");
+  ajax.send();
+}
+
+function frozenSandwich() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option29.html");
+  ajax.send();
+}
+
+function frozenWaffle() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option31.html");
+  ajax.send();
+}
+
+/* level 5 selection options*/
+function pizzaCalzone() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option32.html");
+  ajax.send();
+}
+
+function pasta() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option33.html");
+  ajax.send();
+}
+
+function burger() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option34.html");
+  ajax.send();
+}
+
+function grilled() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option35.html");
+  ajax.send();
+}
+function steak() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option36.html");
+  ajax.send();
+}
+
+function soup() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option37.html");
+  ajax.send();
+}
+
+function homePasta() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option39.html");
+  ajax.send();
+}
+
+function cookPizza() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option38.html");
+  ajax.send();
+}
+
+function apps() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option40.html");
+  ajax.send();
+}
+
+function entree() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option41.html");
+  ajax.send();
+}
+
+function homemade() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option42.html");
+  ajax.send();
+}
+
+function eggs() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option43.html");
+  ajax.send();
+}
+
+function wings() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option44.html");
+  ajax.send();
+}
+
+function other() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option45.html");
+  ajax.send();
+}
+
+function french() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option46.html");
+  ajax.send();
+}
+
+function pancakes() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option47.html");
+  ajax.send();
+}
+
+function burrito() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option48.html");
+  ajax.send();
+}
+
+function nachos() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option49.html");
+  ajax.send();
+}
+
+function detroit() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option50.html");
+  ajax.send();
+}
+
+function tonys() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option51.html");
+  ajax.send();
+}
+
+function beef() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option52.html");
+  ajax.send();
+}
+
+function chicken() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option53.html");
+  ajax.send();
+}
+
+function bbq() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option54.html");
+  ajax.send();
+}
+
+function teryaki() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option55.html");
+  ajax.send();
+}
+
+function jackSand() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option56.html");
+  ajax.send();
+}
+
+function platter() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option57.html");
+  ajax.send();
+}
+
+function jimmy() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option58.html");
+  ajax.send();
+}
+
+function otherSand() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option59.html");
+  ajax.send();
+}
+
+function pancakeMeal() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option60.html");
+  ajax.send();
+}
+
+function mcMuffin() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option61.html");
+  ajax.send();
+}
+
+function toaster() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option62.html");
+  ajax.send();
+}
+
+function microwave() {
+  ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+    document.getElementById("title").innerHTML =
+    this.responseText;
+  }
+  ajax.open("GET", "option63.html");
+  ajax.send();
+}
